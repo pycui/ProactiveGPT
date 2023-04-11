b45c2ef4907951014939ecbd14d4a2b886e42ef1
Piaoyang Cui <bcstyle@gmail.com>
2023-04-10 23:38:27 -0700

    GptBot class, input interface, improvements

---
+++ gpt_gpt.py
@@ -0,0 +1,105 @@
+import asyncio
+import datetime
+import openai
+from typing import List
+from discord.ext import commands
+
+
+def split_long_string(text, max_length=2000):
+    """Split a long string into a list of strings with a maximum length."""
+    words = text.split()
+    result = []
+    current_line = ""
+
+    for word in words:
+        if len(current_line + " " + word) <= max_length:
+            current_line += " " + word
+        else:
+            result.append(current_line.strip())
+            current_line = word
+
+    if current_line:
+        result.append(current_line.strip())
+
+    return result
+
+
+def send_message_to_chatgpt(messages: List[str], message: str, model: str):
+    """
+    send message to OpenAI chat API, get response from OpenAI chat API, and send response to user.
+    """
+    messages.append({"content": message, "role": "user"})
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        max_tokens=2048,  # Adjust the number of tokens as needed
+        temperature=0.8,  # Adjust the creativity level
+    )
+
+    gpt_response = response.choices[0].message.content
+    messages.append({"content": gpt_response, "role": "system"})
+    return gpt_response
+
+
+class GptBot(commands.Bot):
+    def __init__(self, model, task_prompt, loop_prompt, cadence, user_id, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.model = model
+        self.task_prompt = task_prompt
+        self.loop_prompt = loop_prompt
+        self.cadence = cadence
+        self.user_id = user_id
+        self.messages = []
+        self._commands()
+
+    async def send_daily_message(self):
+        loop = asyncio.get_event_loop()
+
+        user = await self.fetch_user(self.user_id)
+        if user is not None:
+            await user.send(f"Your initial instruction to ChatGPT: {self.task_prompt}")
+            await user.send(f"ChatGPT is now generating the plan for you...")
+            gpt_response = await loop.run_in_executor(None, send_message_to_chatgpt, self.messages, self.task_prompt, self.model)
+            if (len(gpt_response) > 2000):
+                for line in split_long_string(gpt_response):
+                    await user.send(line)
+            else:
+                await user.send(gpt_response)
+            _ = await loop.run_in_executor(None, send_message_to_chatgpt, self.messages, self.loop_prompt, self.model)
+
+            while True:
+                now = datetime.datetime.now()
+                seconds_till_next_hour = 3600 - now.minute * \
+                    60 - now.second - now.microsecond / 1_000_000
+                # Add more hours to make up cadence hours.
+                more_hours = (now.hour + 1) % int(self.cadence)
+                seconds_till_next_run = seconds_till_next_hour + more_hours * 3600
+                await asyncio.sleep(seconds_till_next_run)
+
+                time = datetime.datetime.now().strftime("%m/%d/%Y %H:%M:%S")
+                gpt_response = await loop.run_in_executor(None, send_message_to_chatgpt, self.messages, f"SCHEDULER: {time}", self.model)
+                if "nothing to do now" in gpt_response.lower():
+                    continue
+                await user.send(gpt_response)
+
+
+    async def on_ready(self):
+        print(f"Using task_prompt: {self.task_prompt}")
+        print(f"Using loop_prompt: {self.loop_prompt}")
+        print(f"{self.user.name} has connected to Discord!")
+        asyncio.create_task(self.send_daily_message())
+
+    def _commands(self):
+        @self.command(name="hello", help="Says hello to the user.")
+        async def hello(ctx):
+            await ctx.send(f"Hello, {ctx.author.mention}!")
+
+        @self.command(name="echo", help="Repeats the message back to the user.")
+        async def echo(ctx, *, message: str):
+            await ctx.send(message)
+
+        @self.command(name="g", help="Say something to chatgpt.")
+        async def g(ctx, *, message: str):
+            loop = asyncio.get_event_loop()
+            gpt_response = await loop.run_in_executor(None, send_message_to_chatgpt, ctx.bot.messages, message, ctx.bot.model)
+            await ctx.send(gpt_response)
--- proactive_gpt.py
+++ proactive_gpt.py
@@ -4,116 +4,87 @@ from discord.ext import commands
 import asyncio
 import datetime
 import openai
-
-# Set your API key
-openai.api_key = os.environ.get("GPT_KEY")
+import questionary
+from questionary import ValidationError
+from gpt_gpt import GptBot
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-# Replace "!" with the desired prefix for your bot commands.
-bot = commands.Bot(command_prefix="!", intents=intents,
-                   heartbeat_timeout=10000)
-
-TOKEN = os.environ.get("DISCORD_BOT_TOKEN")
-
 messages = []
 
-initial_message = ("You are a personal fitness trainer now. I'm an asian male, 32 years ago, "
-                   "with 5'7\" height and 160 lbs weight. I want to have a 7 day workout plan, using the time in the morning "
+default_task_prompt = ("You are a personal fitness trainer now. I want to have a 7 day workout plan, using the time in the morning "
                    "(9-10AM) and evening (7PM-10PM) for exercises and activities. The workout should be not be too heavy. "
                    "I will start the plan tomorrow (04/06/2023). Can you help me make a plan? The plan should be detailed, with the date and time of each activities. ")
-loop_message = ("Now, I will start following your plan and would like you to remind on what to do at current time. A scheduler will help provide current time info to you. He will always start his words with \"SCHEDULER: \", followed by a date and time, by roughly once per every hour.  If I should be doing an activity at that time, you should answer with: \"Exercise Time!\", followed by the activity. Otherwise, reply with \"Nothing to do now\". If you understand, please say OK.")
+loop_prompt_template = ("Now, I will start following your plan and would like you to remind on what to do at current time. A scheduler will help provide current time info to you. He will always start his words with \"SCHEDULER: \", followed by a date and time, by roughly once per every {cadence} hour.  If I should be doing an activity at that time, you should answer with: \"Exercise Time!\", followed by the activity. Otherwise, reply with \"Nothing to do now\". If you understand, please say OK.")
 
 
-def send_message_to_chatgpt(message: str):
-    """
-    send message to OpenAI chat API, get response from OpenAI chat API, and send response to user.
-    """
-    global messages
-    messages.append({"content": message, "role": "user"})
-    response = openai.ChatCompletion.create(
-        model="gpt-4",
-        messages=messages,
-        max_tokens=2048,  # Adjust the number of tokens as needed
-        temperature=0.8,  # Adjust the creativity level
-    )
+def cadence_validator(value: str):
+    try:
+        value_int = int(value)
+    except ValueError:
+        raise ValidationError(message="Please enter a valid integer.")
 
-    gpt_response = response.choices[0].message.content
-    messages.append({"content": gpt_response, "role": "system"})
-    return gpt_response
+    if value_int <= 0:
+        raise ValidationError(message="Please enter a positive integer.")
+
+    return True
 
 
-def split_long_string(text, max_length=2000):
-    """Split a long string into a list of strings with a maximum length."""
-    words = text.split()
-    result = []
-    current_line = ""
+def main():
+    # Select model.
+    choices = [
+        "gpt-4",
+        "gpt-3.5-turbo",
+    ]
 
-    for word in words:
-        if len(current_line + " " + word) <= max_length:
-            current_line += " " + word
-        else:
-            result.append(current_line.strip())
-            current_line = word
+    model = questionary.select(
+        "Please choose your GPT model:", choices=choices).ask()
 
-    if current_line:
-        result.append(current_line.strip())
+    if model:
+        print(f"You selected: {model}")
+    else:
+        print("No model was selected. Exiting.")
+        return
 
-    return result
+    # Enter API key.
+    api_token = questionary.text("Please enter your OpenAI API token. \nIf empty, I will use the OPENAI_API_KEY environment variable:").ask()
 
+    if api_token:
+        print("Using user-provided API key.")
+        openai.api_key = api_token
+    else:
+        print("Using environment variable OPENAI_API_KEY.")
+        openai.api_key = os.environ.get("GPT_KEY")
 
-async def send_daily_message(user_id):
-    loop = asyncio.get_event_loop()
+    # Enter discord bot token.
+    discord_token = questionary.text("Please enter your Discord bot token. \nIf empty, I will use the DISCORD_BOT_TOKEN environment variable:").ask()
+    if discord_token:
+        print("Using user-provided Discord bot token.")
+    else:
+        print("Using environment variable DISCORD_BOT_TOKEN.")
+        discord_token = os.environ.get("DISCORD_BOT_TOKEN")
 
-    user = await bot.fetch_user(user_id)
-    if user is not None:
-        await user.send(f"Your initial instruction to ChatGPT: {initial_message}")
-        gpt_response = await loop.run_in_executor(None, send_message_to_chatgpt, initial_message)
-        if (len(gpt_response) > 2000):
-            for line in split_long_string(gpt_response):
-                await user.send(line)
-        else:
-            await user.send(gpt_response)
-        _ = await loop.run_in_executor(None, send_message_to_chatgpt, loop_message)
+    # Enter cadence.
+    cadence = questionary.text("Please enter the cadence of your GPT's proactiveness (in hours, cron format will be supported soon):",
+                               validate=cadence_validator, default="1").ask()
 
-        while True:
-            now = datetime.datetime.now()
-            seconds_till_next_hour = 3600 - now.minute * \
-                60 - now.second - now.microsecond / 1_000_000
-            await asyncio.sleep(seconds_till_next_hour)
+    # Enter task prompt.
+    task_prompt = questionary.text("Please enter your task prompt to GPT:", default=default_task_prompt).ask()
+    loop_prompt = loop_prompt_template.format(cadence=cadence)
 
-            time = datetime.datetime.now().strftime("%m/%d/%Y %H:%M:%S")
-            gpt_response = await loop.run_in_executor(None, send_message_to_chatgpt, f"SCHEDULER: {time}")
-            if "nothing to do now" in gpt_response.lower():
-                continue
-            await user.send(gpt_response)
+    # Enter loop prompt.
+    loop_prompt = questionary.text("Please enter your loop prompt to GPT:", default=loop_prompt).ask()
 
+    # Enter user ID.
+    user_id = questionary.text("Please enter the discord user ID to send the message to:").ask()
 
-@bot.event
-async def on_ready():
-    print(f"{bot.user.name} has connected to Discord!")
-    # Replace 'user_id' with the user's ID you want to send a message to
-    user_id = '740804964913250344'
-    # await send_message_to_user(user_id)
-    asyncio.create_task(send_daily_message(user_id))
+    # Replace "!" with the desired prefix for your bot commands.
+    bot = GptBot(model, task_prompt, loop_prompt, cadence, user_id, command_prefix="!", intents=intents,
+                    heartbeat_timeout=10000)
 
-
-@bot.command(name="hello", help="Says hello to the user.")
-async def hello(ctx):
-    await ctx.send(f"Hello, {ctx.author.mention}!")
+    bot.run(discord_token)
 
 
-@bot.command(name="echo", help="Repeats the message back to the user.")
-async def echo(ctx, *, message: str):
-    await ctx.send(message)
-
-
-@bot.command(name="g", help="Say something to chatgpt.")
-async def g(ctx, *, message: str):
-    loop = asyncio.get_event_loop()
-    gpt_response = await loop.run_in_executor(None, send_message_to_chatgpt, message)
-    await ctx.send(gpt_response)
-
-
-bot.run(TOKEN)
+if __name__ == "__main__":
+    main()
